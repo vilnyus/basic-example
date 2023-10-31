024d8fd36e0986bc089a1fe60dc4c27fcc1f6af4
vagharsh <govagharsh@gmail.com>
2023-10-31 12:59:45 +0400

    with Openai part

--- index.js
+++ index.js
@@ -8,26 +8,71 @@ const appSettings = {
 const app = initializeApp(appSettings);
 
 const database = getDatabase(app);
+
 const groceriesInDb = ref(database, "shopping");
 
-const userInput = document.getElementById("user-input");
-const button = document.getElementById("add-button");
-const buttonContainer = document.getElementById("buttonContainer");
+const userInput         = document.getElementById("user-input");
+const button            = document.getElementById("add-button");
 
-button.addEventListener("click", function(){
+
+button.addEventListener("click", function() {
     
     // Get the input value
-    var buttonName = document.getElementById("user-input").value;    
+    var inputString = document.getElementById("user-input").value;    
 
-    if (buttonName) {
-        // Create new buttons with similar names
-        for (var i = 1; i <= 3; i++) {
-            var newButton = document.createElement("button");
-            newButton.innerHTML = buttonName + " " + i;
-            document.getElementById("buttonContainer").appendChild(newButton);
-            buttonContainer.appendChild(newButton);
-        }
+    if (inputString) {
+        var requestData = {
+            prompt: inputString,
+            max_token: 50,
+            n: 3
+        };
+
+        axios.post("https://api.openai.com/v1/engines/text-davinci-003/completions", requestData, { 
+            headers : {
+                "Authorization": "Bearer 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",
+                "Content-Type": "application/json"
+            }
+        }).then(function(response){
+            var generatedStrings = response.data.choices.map(choice=>choice.text);
+
+            var buttonContainer = document.getElementById("buttonContainer");
+
+            buttonContainer.innerHTML = "";
+            
+            generatedStrings.array.forEach(function (generatedString, index) {
+                var newButton = document.createElement("button");
+                newButton.innerHTML = generatedString;
+
+                newButton.addEventListener("click", function() {
+                    alert("Button " + (index + 1) + " was clicked!");
+                });
+
+                buttonContainer.appendChild(newButton);                
+            });
+        })
+        .catch(function(error){
+            console.log(error);
+        });
     }
     push(groceriesInDb, userInput.value);
 });
 
+
+// // const buttonContainer   = document.getElementById("buttonContainer");
+
+// button.addEventListener("click", function(){
+    
+//     // Get the input value
+//     var buttonName = document.getElementById("user-input").value;    
+
+//     if (buttonName) {
+//         // Create new buttons with similar names
+//         for (var i = 1; i <= 3; i++) {
+//             var newButton = document.createElement("button");
+//             newButton.innerHTML = buttonName + " " + i;
+//             document.getElementById("buttonContainer").appendChild(newButton);
+//             buttonContainer.appendChild(newButton);
+//         }
+//     }
+//     push(groceriesInDb, userInput.value);
+// });
